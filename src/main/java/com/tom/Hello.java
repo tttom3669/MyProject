dd3d4138642949d5a941c170da8e04fc9d1c5abe
John Chen <tttom3669@gmail.com>
2021-04-21 16:19:38 +0800

    Initial commit

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,11 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//        new Person().hello();
+        Person p = new Person();
+        p.hello();
+
+    }
+}
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,13 @@
+package com.tom
+
+fun main() {
+//    println("Hello kotlin");
+//    Human().hello();
+    val h = Human();
+    h.hello();
+}
+class  Human{
+    fun hello(){
+        println("Hello Kotlin");
+    }
+}
