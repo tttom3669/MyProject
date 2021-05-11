64e3a662710aa3ab95a314446ed66386a7c2356a
John Chen <tttom3669@gmail.com>
2021-05-11 17:21:09 +0800

    Java and Kotlin variable ,data type

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -5,6 +5,14 @@ fun main() {
 //    Human().hello();
     val h = Human();
     h.hello();
+    var age : Int = 19;
+    val age2 = 20;//自動推斷型態
+    age = 21;
+    var weight = 66.5f;
+    var name : String
+    name ="Hank";
+
+
 }
 class  Human{
     fun hello(){
